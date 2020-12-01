b48f9f3555d0f9b94e4be291178c7ea3a19bc1ca
JEROME TORRES <jtorres175@cnm.edu>
2020-11-30 17:16:19 -0700

    11-30-20 Before SignalR package download and chathub setup

---
+++ WebsitePortfolioJTorres/Server/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsitePortfolioJTorres.Server.Hubs
+{
+    public class ChatHub
+    {
+    }
+}
